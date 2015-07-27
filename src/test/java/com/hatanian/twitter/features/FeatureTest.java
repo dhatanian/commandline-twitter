051af8d742a44b569ad6472e3908eda265416800
David Hatanian <david.hatanian@revevol.eu>
2015-07-27 18:24:17 +0200

    More readable feature test

---
+++ src/test/java/com/hatanian/twitter/features/FeatureTest.java
@@ -0,0 +1,84 @@
+package com.hatanian.twitter.features;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.hatanian.twitter.App;
+import com.hatanian.twitter.ProgramTerminator;
+import com.hatanian.twitter.command.persistence.PostRepository;
+import com.hatanian.twitter.console.Console;
+import com.hatanian.twitter.output.Output;
+import org.junit.Before;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+public class FeatureTest {
+    public static final long EXECUTION_INSTANT = 1437776421000L;
+    @Mock
+    Console console;
+    @Mock
+    Output out;
+    @Mock
+    ProgramTerminator programTerminator;
+    ChangingClock clock = new ChangingClock(EXECUTION_INSTANT);
+    PostRepository postRepository = new PostRepository(clock);
+    private int lineIndex;
+    private List<TestUserInput> userInputList = new LinkedList<>();
+    private InOrder inOrder;
+
+    @Before
+    public void setUp() throws Exception {
+        lineIndex = 0;
+        inOrder = inOrder(out);
+        when(console.readLine()).thenAnswer(invocation -> {
+            if (lineIndex >= userInputList.size()) {
+                throw new IllegalArgumentException("Unexpected command number " + 0);
+            } else {
+                TestUserInput testUserInput = userInputList.get(lineIndex);
+                lineIndex++;
+                clock.setCurrentTime(testUserInput.time);
+                return testUserInput.userInput;
+            }
+        });
+    }
+
+    protected void registerUserInput(String userInput, int age, TimeUnit ageUnit) {
+        userInputList.add(new TestUserInput(userInput, EXECUTION_INSTANT - ageUnit.toMillis(age)));
+    }
+
+    private class TestUserInput {
+        private String userInput;
+        private long time;
+
+        public TestUserInput(String userInput, long time) {
+            this.userInput = userInput;
+            this.time = time;
+        }
+    }
+
+    protected void runApp() {
+        Injector injector = Guice.createInjector(new TestGuiceModule(console, out, programTerminator, clock, postRepository));
+        App app = injector.getInstance(App.class);
+        app.run();
+    }
+
+
+    protected void expectOutput(String line) {
+        inOrder.verify(out).println(line);
+    }
+
+    protected void expectUserAskedForCommand(int times) {
+        inOrder.verify(out, times(times)).print("> ");
+    }
+
+    protected void verifyNoMoreOutput() {
+        inOrder.verifyNoMoreInteractions();
+    }
+}
--- src/test/java/com/hatanian/twitter/features/TimelineFeature.java
+++ src/test/java/com/hatanian/twitter/features/TimelineFeature.java
@@ -1,74 +1,35 @@
 package com.hatanian.twitter.features;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.hatanian.twitter.App;
-import com.hatanian.twitter.ProgramTerminator;
-import com.hatanian.twitter.command.persistence.PostRepository;
-import com.hatanian.twitter.console.Console;
-import com.hatanian.twitter.output.Output;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TimelineFeature {
-    public static final long EXECUTION_INSTANT = 1437776421000L;
-    @Mock
-    Console console;
-    @Mock
-    Output out;
-    @Mock
-    ProgramTerminator programTerminator;
-
-    ChangingClock clock = new ChangingClock(EXECUTION_INSTANT);
-    PostRepository postRepository = new PostRepository(clock);
-    int lineIndex;
-
-    @Before
-    public void setUp() throws Exception {
-        lineIndex = 0;
-    }
+public class TimelineFeature extends FeatureTest {
 
     @Test
     public void shouldDisplayTimelineWithAddedPostsAndExit() throws Exception {
-        when(console.readLine()).thenAnswer(invocation -> {
-            switch (lineIndex++) {
-                case 0:
-                    clock.setCurrentTime(EXECUTION_INSTANT - 60 * 1000);
-                    return "Alice -> Post 1";
-                case 1:
-                    clock.setCurrentTime(EXECUTION_INSTANT - 10);
-                    return "Alice -> Post 2";
-                case 2:
-                    return "Bob -> Unrelated post";
-                case 3:
-                    clock.setCurrentTime(EXECUTION_INSTANT);
-                    return "Alice";
-                case 4:
-                    return "exit";
-                default:
-                    throw new IllegalArgumentException("Unexpected command number " + 0);
-            }
-        });
+        registerUserInput("Alice -> Post 1", 1, TimeUnit.MINUTES);
+        registerUserInput("Alice -> Post 2", 10, TimeUnit.MILLISECONDS);
+        registerUserInput("Bob -> Unrelated post", 10, TimeUnit.MILLISECONDS);
+        registerUserInput("Alice", 0, TimeUnit.MILLISECONDS);
+        registerUserInput("exit", 0, TimeUnit.MILLISECONDS);
 
-        Injector injector = Guice.createInjector(new TestGuiceModule(console, out, programTerminator, clock, postRepository));
-        App app = injector.getInstance(App.class);
-        app.run();
+        runApp();
 
-        InOrder inOrder = inOrder(out);
-        inOrder.verify(out).println("Welcome on this Command Line social network !");
-        inOrder.verify(out, times(4)).print("> ");
-        inOrder.verify(out).println("Post 2 (moments ago)");
-        inOrder.verify(out).println("Post 1 (1 minute ago)");
-        inOrder.verify(out).print("> ");
-        inOrder.verify(out).println("Thank you, good bye !");
-        inOrder.verifyNoMoreInteractions();
+        expectOutput("Welcome on this Command Line social network !");
+        expectUserAskedForCommand(4);
+        expectOutput("Post 2 (moments ago)");
+        expectOutput("Post 1 (1 minute ago)");
+        expectUserAskedForCommand(1);
+        expectOutput("Thank you, good bye !");
+        verifyNoMoreOutput();
     }
 
 }
